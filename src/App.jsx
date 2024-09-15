857733ea5f89408a76e80b40bfb01f6549600e18
Dipen Patel <107390824+DOodle25@users.noreply.github.com>
2024-09-16 01:02:10 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import Parallax from "../components/parallax";
+import Aboutme from "../components/Aboutme";
+import Timeline from "../components/timeline";
+import Skills from "../components/skills"
+import Socials from "../components/socials"
+import Footer from "../components/footer";
+function App() {
+  return (
+    <>
+      <Parallax />
+      <Aboutme />
+      <Timeline />
+      <Socials />
+      <Skills />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
